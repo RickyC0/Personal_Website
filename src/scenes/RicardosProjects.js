72a786eed5dfb518c0cb4c5a4b6356f02e8921c9
Ricardo Raji Chahine <ricardochahine0@gmail.com>
2025-08-05 04:46:09 -0400

    Fixed resizing logic for RicardosProjects world

--- src/scenes/RicardosProjects.js
+++ src/scenes/RicardosProjects.js
@@ -6,9 +6,6 @@ export class RicardosProjects extends Phaser.Scene{
     constructor(){
         super({key: 'RicardosProjects'});
         this.CLOUDMOVEMENTSPEED = 64; //pixels
-
-        this.MAPOFFSETX= 650;
-        this.MAPOFFSETY= -350;
     }
     
     init(data){
@@ -16,6 +13,95 @@ export class RicardosProjects extends Phaser.Scene{
     }
 
     create(){
+      this.map = this.make.tilemap({ key: 'ricardosProjectsMap' });
+      this.movingCloudsBackground = this.make.tilemap({ key: 'movingCloudsMap' });
+
+      const cloudsTileset = this.map.addTilesetImage('clouds-sprites-smaller', 'clouds-spritesheet');
+      const colorsTileset = this.map.addTilesetImage('Color Palette', 'colors-spritesheet');
+      const mainTileset = this.map.addTilesetImage('MainTilesetPNG', 'tileset')
+
+
+      //----------------------DISPLAYING THE BACKGROUND------------------------------------
+      // Display the layers of the background map
+      this.backgroundLayerMap = {};
+      this.movingCloudsBackground.layers.forEach(layerData => {
+          const layer = this.movingCloudsBackground.createLayer(
+              layerData.name,
+              [cloudsTileset, colorsTileset], //use both tilesets here
+              0, //X-offset 
+              0  //Y-offset
+          );
+
+        this.backgroundLayerMap[layerData.name] = layer;
+      });
+      // -----------------------------------------------------
+      
+
+      //----------------------DISPLAYING THE MAP------------------------------------
+
+      // Display the layers of the main clouds map
+      this.cloudsLayerMap = {};
+      this.map.layers.forEach(layerData => {
+          const layer = this.map.createLayer(
+              layerData.name,
+              [cloudsTileset, colorsTileset, mainTileset], // use both tilesets here
+              0, //X-offset 
+              0 //Y-offset
+          );
+
+          this.cloudsLayerMap[layerData.name] = layer;
+      });
+     
+      //---------------------------------------------------------
+      
+      //Offset of the background layer TODO VERIFY
+      this.cloudOffsetX = 0;
+
+      this.collisionLayer = this.map.getObjectLayer('Collision')?.objects || [];
+      this.interactableObjects = this.map.getObjectLayer('Interactables')?.objects || [];
+
+
+      // ------------- PLAYER & CAMERA -----------------------------------------
+
+      // place the player
+      this.player = new Player(this, 0, 0);
+
+      this.cameras.main
+        .startFollow(
+          this.player,
+          false, // roundPixels
+          0.1,   // lerpX
+          0.1    // lerpY
+        );
+      
+      //---------------------------------------------------------
+
+      this.cursors = this.input.keyboard.createCursorKeys();
+
+      this.keys = this.input.keyboard.addKeys({
+            up: Phaser.Input.Keyboard.KeyCodes.W,
+            down: Phaser.Input.Keyboard.KeyCodes.S,
+            left: Phaser.Input.Keyboard.KeyCodes.A,
+            right: Phaser.Input.Keyboard.KeyCodes.D
+        });
+      //----------------------------------------------------
+
+      //-------------------------------Collision and Interactable layers---------------------------
+      this._createCollisionShapes();
+      this._createInteractableZones();
+
+      // -------------------------------------------------------
+      // SCALE & CENTER THE CLOUD‐BACKGROUND (detached from camera)
+      // -------------------------------------------------------
+      this.scale.on('resize', this._onResize, this);
+
+      //Scale and center now at the start of the scene
+      this._onResize();
+
+      
+
+
+      //-------------------------------TRANSITION INTO THE SCENE--------------------------------
       //Transition that opens the scene
       const transition = this.add.image(
         this.scale.width/2,
@@ -24,7 +110,7 @@ export class RicardosProjects extends Phaser.Scene{
       )
         .setOrigin(0.5)
         .setScrollFactor(0)
-        .setDisplaySize(this.scale.width * 2.5, this.scale.height * 2.5)
+        .setDisplaySize(this.scale.width * 2, this.scale.height * 2.5)
         .setDepth(1000)// on top of everything
         .setAlpha(1);// start fully opaque
 
@@ -37,193 +123,144 @@ export class RicardosProjects extends Phaser.Scene{
         onComplete: () => transition.destroy()
       });
 
-      this.map = this.make.tilemap({ key: 'ricardosProjectsMap' });
-      this.movingCloudsBackground = this.make.tilemap({ key: 'movingCloudsMap' });
+    }
 
-      const cloudsTileset = this.map.addTilesetImage('clouds-sprites-smaller', 'clouds-spritesheet');
-      const colorsTileset = this.map.addTilesetImage('Color Palette', 'colors-spritesheet');
-      const mainTileset = this.map.addTilesetImage('MainTilesetPNG', 'tileset')
+    _createCollisionShapes(){
+      this.collisionShapes = [];
+      this.collisionLayer.forEach(obj => {
+        // “orig” coords
+        const origX = obj.x + obj.width/2;
+        const origY = obj.y + obj.height/2;
 
-      // Display the layers of the background map
-      this.backgroundLayerMap = {};
-      this.movingCloudsBackground.layers.forEach(layerData => {
-          const layer = this.movingCloudsBackground.createLayer(
-              layerData.name,
-              [cloudsTileset, colorsTileset], //use both tilesets here
-              0, //X-offset 
-              0  //Y-offset
-          );
-        // detach from camera movement
-        layer.setScrollFactor(0);
-
-        this.backgroundLayerMap[layerData.name] = layer;
-      });
-      // -----------------------------------------------------
-
-      //CENTERING THE MAP
-      // --------------------------------------------------------
-      //TODO
-      
-
-      //----------------------------------------------------------
-
-      // Display the layers of the main clouds map
-      this.cloudsLayerMap = {};
-      this.map.layers.forEach(layerData => {
-          const layer = this.map.createLayer(
-              layerData.name,
-              [cloudsTileset, colorsTileset, mainTileset], // use both tilesets here
-              this.MAPOFFSETX, //X-offset 
-              this.MAPOFFSETY  //Y-offset
-          );
-
-          this.cloudsLayerMap[layerData.name] = layer;
-      });
-
-      
-    
-
-      //Offset of the background layer TODO VERIFY
-      this.cloudOffsetX = 0;
-
-
-      //TODO: SOME RESIZING FUNCTION THAT SCALES BOTH THE BACKGROUND AND THE MAP, BUT SEPARATLY, SINCE THEY HAVE DIFFERENT SIZES
-      this.applyResize(this, this.map);
-      this.applyResize(this, this.movingCloudsBackground);
-
-      this.resizeHandler = () => {
-          this.applyResize(this, this.map);
-          this.applyResize(this, this.movingCloudsBackground);
-      }
-      this.scale.on('resize', this.resizeHandler);
-
-      this.events.once(Phaser.Scenes.Events.SHUTDOWN, () => {
-          this.scale.off('resize', this.resizeHandler);
-      });
-
-      //Camera Movements lIMIT
-      this.cameras.main.setBounds(
-        this.MAPOFFSETX/2,
-        this.MAPOFFSETY/2,
-        this.map.widthInPixels,
-        this.map.heightInPixels
-      );
-
-      //Setting bounds to the map
-      
-      this.physics.world.setBounds(
-        this.MAPOFFSETX, 
-        this.MAPOFFSETY,
-        this.map.widthInPixels,
-        this.map.heightInPixels
-      );
-
-      //TODO: CHANGE PLAYER SPAWN COORDINATES TO MAKE THEM NOT HARDCODED
-      this.player = new Player(this, 60*16, 50*16);
-
-      this.cursors = this.input.keyboard.createCursorKeys();
-
-      this.keys = this.input.keyboard.addKeys({
-            up: Phaser.Input.Keyboard.KeyCodes.W,
-            down: Phaser.Input.Keyboard.KeyCodes.S,
-            left: Phaser.Input.Keyboard.KeyCodes.A,
-            right: Phaser.Input.Keyboard.KeyCodes.D
-        });
-      //----------------------------------------------------
-
-      //Camera MOvements to follow the player
-      //--------------------------------------------
-      this.cameras.main.startFollow(
-        this.player,    
-        false, // roundPixels
-        0.1, // lerpX (0 = no follow in X, 1 = lock perfectly)
-        0.1  // lerpY (0 = no follow in Y, 1 = lock perfectly)
-      );
-
-      //Collision with object and limit to the map
-      //----------------------------------------------------
-      const collisionLayer = this.map.getObjectLayer('Collision');
-
-      this.collision = this.physics.add.staticGroup(); // Create group for collision
-
-      if (collisionLayer) {
-        collisionLayer.objects.forEach(obj => {
-
-        let shape;
-    
-        shape = this.add.rectangle(
-        obj.x + obj.width / 2 + this.MAPOFFSETX,
-        obj.y + obj.height / 2 +this.MAPOFFSETY,
-        obj.width,
-        obj.height,
-        0, //color of the borders
-        0 //opacity
+        // place in map coords
+        const shape = this.add.rectangle(
+          origX + this.worldOriginX_map,
+          origY + this.worldOriginY_map,
+          obj.width,
+          obj.height,
+          0, 0
         );
+        this.physics.add.existing(shape, true);
+        shape.origX = origX;
+        shape.origY = origY;
 
-        this.physics.add.existing(shape, true); // Make it static
-        this.collision.add(shape); // Add to group
+        this.collisionShapes.push(shape);
+      });
+      this.physics.add.collider(this.player, this.collisionShapes);
+    }
+
+    // build InteractableRect instances exactly once
+    _createInteractableZones(){
+      this.interactableZones = [];
+      this.interactableObjects.forEach(obj => {
+        const origX = obj.x + obj.width/2;
+        const origY = obj.y + obj.height/2;
+        const zone = new InteractableRect(
+          this,
+          obj,
+          origX + this.worldOriginX_map,
+          origY + this.worldOriginY_map,
+          this.previousScene
+        );
+        zone.origTileX = origX;
+        zone.origTileY = origY;
+        this.interactableZones.push(zone);
+      });
+    }
+
+    // called on startup & whenever the canvas resizes
+    _onResize() {
+      // 1) sizes & origins
+      this.screenWidth      = this.scale.width;
+      this.screenHeight     = this.scale.height;
+      this.mapWidth         = this.map.widthInPixels;
+      this.mapHeight        = this.map.heightInPixels;
+      this.backgroundWidth  = this.movingCloudsBackground.widthInPixels;
+      this.backgroundHeight = this.movingCloudsBackground.heightInPixels;
+
+      this.worldOriginX_map = (this.screenWidth  - this.mapWidth)  / 2;
+      this.worldOriginY_map = (this.screenHeight - this.mapHeight) / 2;
+      this.worldOriginX_bg  = (this.screenWidth  - this.backgroundWidth)  / 2;
+      this.worldOriginY_bg  = (this.screenHeight - this.backgroundHeight) / 2;
+
+      // 2) reposition static background
+      for (const layer of Object.values(this.backgroundLayerMap)) {
+        layer.setPosition(this.worldOriginX_bg, this.worldOriginY_bg)
+            .setScrollFactor(0);
+      }
+
+      // 3) reposition main tilemap
+      for (const layer of Object.values(this.cloudsLayerMap)) {
+        layer.setPosition(this.worldOriginX_map, this.worldOriginY_map)
+            .setScrollFactor(1);
+      }
+
+      
+      //--------------------- COLLISION AND MAP LIMIT-------------------------------------------------------
+      // 4) reposition & refresh collisions
+      if (this.collisionShapes) {
+        this.collisionShapes.forEach(shape => {
+          shape.x = shape.origX + this.worldOriginX_map;
+          shape.y = shape.origY + this.worldOriginY_map;
+          shape.body.updateFromGameObject();
         });
       }
-      
 
-      this.physics.add.collider(this.player, this.collision);
+      // 5) reposition & reset interactable zones
+      if (this.interactableZones) {
+        this.interactableZones.forEach(zone => {
+          zone.setPosition(
+            zone.origTileX + this.worldOriginX_map,
+            zone.origTileY + this.worldOriginY_map
+          );
+          zone.body.reset(zone.x, zone.y);
+        });
+      }
 
-      //Adding interactable objects' logic
-      // 1) set up “press Enter” key once
-      this.interactKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+      // 6) move the player to bottom‐middle again
+      const spawnX = this.worldOriginX_map + this.mapWidth/2;
+      const spawnY = this.worldOriginY_map + this.mapHeight - 10 * (this.map.tileHeight/2);
+      this.player.setPosition(spawnX, spawnY);
 
-      // 2) group to keep references
-      this.interactables = this.add.group();
+      // 7) camera & physics bounds
+      this.cameras.main
+        .setBounds(
+          0,
+          0,
+          this.mapWidth,
+          this.mapHeight
+        )
+        .setSize(this.screenWidth, this.screenHeight)
+        .startFollow(this.player, false, 0.1, 0.1);
 
-      // 3) pull the ractangles to interact with from the Interactables object layer
-      const interactableObjects = this.map.getObjectLayer('Interactables')?.objects || [];
-
-      interactableObjects.forEach(obj => {
-        // compute world‐space x/y exactly as your map layers use it
-        const worldX = obj.width/2 + obj.x + this.MAPOFFSETX;
-        const worldY = obj.height/2 + obj.y + this.MAPOFFSETY;
-        const zone   = new InteractableRect(this, obj, worldX, worldY,this.previousScene);
-        this.interactables.add(zone);
-      });
-
-
+      this.physics.world.setBounds(
+        this.worldOriginX_map,
+        this.worldOriginY_map,
+        this.mapWidth,
+        this.mapHeight
+      );
     }
-
-    //TODO: CHANGE THIS FUNCTION BECAUSE THIS IS INCORRECT
-    applyResize(scene, tilemap) {
-        const gameSize = scene.scale.gameSize;
-        const zoom = gameSize.width  / tilemap.heightInPixels;
-
-        scene.cameras.main.setZoom(zoom);
-
-        const centerX = tilemap.widthInPixels  / 2;
-        const centerY = tilemap.heightInPixels / 2;
-        scene.cameras.main.centerOn(centerX, centerY);
-    }
+  
 
     moveCloudsRight(delta){
       // 1) advance the offset (px per second)
       this.cloudOffsetX += this.CLOUDMOVEMENTSPEED * (delta / 1000);
 
-      // 2) grab screen width
-      const screenWidth   = this.scale.gameSize.width;
-      const cloudMapWidth = this.movingCloudsBackground.widthInPixels;
-
       // 3) how much extra map we have beyond one screen
-      const wrapDistance = cloudMapWidth - screenWidth;
+      const wrapDistance =  this.backgroundWidth - this.screenWidth;
 
       // 4) wrap that extra bit only
       //    (ensure positive remainder even if cloudOffsetX overshoots)
       this.cloudOffsetX = ((this.cloudOffsetX % wrapDistance) + wrapDistance) % wrapDistance;
 
       // 5) baseX = flush‑right on screen when offset = 0
-      const baseX   = screenWidth - cloudMapWidth;
+      const baseX = this.screenWidth - this.backgroundWidth;
 
       // 6) final x = baseX + your animated offset
       const targetX = baseX + this.cloudOffsetX;
 
       //TODO: CHANGE THIS TO MAKE IT NOT HARDCODED
-      const targetY = -50;
+      const targetY = 0;
 
       // 7) apply to every cloud layer
       for (const layer of Object.values(this.backgroundLayerMap)) {
